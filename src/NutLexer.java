862cd7602eec10c592eaf16e205e24aed7893c4f
Unknown <julian_decastro@dlsu.edu.ph>
2019-10-28 21:55:29 +0800

    added error listener to main class

--- src/NutLexer.java
+++ src/NutLexer.java
@@ -1,4 +1,4 @@
-// Generated from D:/School/CMPILER/MP/CMPILER_NUT\NutLexer.g4 by ANTLR 4.7.2
+// Generated from C:/Users/GeneralTank/Documents/4th Year 1st Term/CMPILER/CMPILER_NUT\NutLexer.g4 by ANTLR 4.7.2
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
--- src/ParserTree.java
+++ src/ParserTree.java
@@ -4,19 +4,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import org.antlr.v4.gui.TreeViewer;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 public class ParserTree {
     public static void main(String[] args) throws IOException {
         //prepare token stream
-        CharStream stream = CharStreams.fromFileName("D:\\School\\CMPILER\\MP\\CMPILER_NUT\\parser_test_case.txt");
+        CharStream stream = CharStreams.fromFileName("C:\\Users\\GeneralTank\\Documents\\4th Year 1st Term\\CMPILER\\CMPILER_NUT\\parser_test_case.txt");
         NutLexer lexer  = new NutLexer(stream);
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(new NutErrorListener());
         TokenStream tokenStream = new CommonTokenStream(lexer);
         NutParser parser = new NutParser(tokenStream);
+        parser.removeErrorListeners();
+        parser.addErrorListener(new NutErrorListener());
         ParseTree tree = parser.startCode();
 
         //show AST in console
@@ -28,6 +30,32 @@ public class ParserTree {
         TreeViewer viewr = new TreeViewer(Arrays.asList(
                 parser.getRuleNames()),tree);
         viewr.open();
+
+        NutErrorListener errorListener = new NutErrorListener();
+
+
+
+    }
+    static class NutErrorListener extends ConsoleErrorListener{
+
+        @Override
+        public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+                throws ParseCancellationException {
+            String err = msg.split("'")[1];
+            if(msg.contains("missing"))
+                System.err.println("Syntax Error at Line " + line + " missing " + err);
+            else if(msg.contains("extraneous input"))
+                System.err.println("Syntax Error at Line " + line + " extra character/s " + err);
+            else if(msg.contains("mismatched input"))
+                System.err.println("Syntax Error at Line " + line + " unexpected " + err);
+            else
+                System.err.println(msg);
+
+        }
+
     }
 
+
 }
+
+
